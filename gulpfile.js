67266757c8cd3466273e4899f97e0e543accf3fa
pure-js <evolution.is.already.underway@gmail.com>
2016-11-23 23:08:12 +0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,29 @@
 const gulp = require('gulp'),
-  spritesmith = require('gulp.spritesmith'),
-  stylus = require('gulp-stylus'),
   merge = require('merge-stream'),
-  csslint = require('gulp-csslint');
+  plugins = require('gulp-load-plugins')();
 
 
 const paths = {
   sprite: [
-    '../dellin-forms/_date/img/calendar.png',
+    // '../dellin-forms/_date/img/calendar.png',
     'static/img/icons-source/**/*.png'
   ],
   stylus: 'stylesheets/interjacent.styl',
   dist: 'dist/'
 };
 
-gulp.task( 'css', function() {
-  return gulp.src( paths.stylus )
-    .pipe(stylus({
+gulp.task( 'css', () =>
+  gulp.src(paths.stylus)
+    .pipe(plugins.stylus({
       'include css': true
     }))
-    .pipe(gulp.dest(paths.dist + 'css'));
-});
+    .pipe(gulp.dest(paths.dist + 'css'))
+);
 
 gulp.task( 'sprite', function() {
   // Generate our spritesheet
   let spriteData = gulp.src( paths.sprite ) // path to images for sprite
-    .pipe(spritesmith({
+    .pipe(plugins.spritesmith({
       imgName: 'icon-sprite.png',
       cssName: '_icon-sprite.styl',
       cssVarMap: function (sprite) {
@@ -42,10 +40,10 @@ gulp.task( 'sprite', function() {
   return merge(imgStream, cssStream);
 });
 
-gulp.task( 'test', [ 'css' ], function() {
-  return gulp.src( paths.dist + 'css/interjacent.css')
-    .pipe(csslint())
-    .pipe(csslint.reporter());
-});
+gulp.task( 'test', gulp.series('css', () =>
+  gulp.src( paths.dist + 'css/interjacent.css')
+    .pipe(plugins.csslint())
+    .pipe(csslint.reporter())
+));
 
-gulp.task( 'default', [ 'sprite', 'css' ] );
+gulp.task( 'default', gulp.parallel('sprite', 'css'));
